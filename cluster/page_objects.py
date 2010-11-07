58a5cfac638217c4b972dcfe923397560d7f898d
Austin Godber <godber@uberhip.com>
2010-11-07 14:34:18 -0500

    redid the tests using PageObjects, I am still uncertain if this is how I want to do it, or even if I am doing it right, for instance the tests.py contains the string matching, perhaps that should be in the PageObjects also

---
+++ cluster/page_objects.py
@@ -0,0 +1,57 @@
+# toying with the idea of using PageObjects for testing purposes, but I am not
+# sure I am getting it quite yet.  Inspired by this talk a columbus code camp.
+# http://www.cheezyworld.com/2010/10/17/slides-from-columbus-code-camp/
+
+class LoginPage(object):
+    def __init__(self, client):
+        self.url = '/accounts/login/'
+        self.client = client
+
+    def visit(self):
+        self.response = self.client.get(self.url)
+
+    def login(self, username, password, nexturl=''):
+        """
+        If nexturl is provided a 302 redirect is issues to that URL after
+        a successful login.
+        """
+        if nexturl == '':
+            url = self.url
+        else:
+            url = self.url + '?next=' + nexturl
+        self.response = self.client.post(url, {
+            'username': username,
+            'password': password,
+        })
+
+
+class DashboardPage(object):
+    def __init__(self, client):
+        self.url = '/'
+        self.client = client
+
+    def launch(self, cluster_id):
+        # FIXME: This should be a POST
+        response = self.client.get('/cluster/' + cluster_id + '/launch')
+        return LaunchPage(self.client, response)
+
+    def get_history(self, cluster_id):
+        response = self.client.get('/cluster/' + cluster_id + '/history')
+        return HistoryPage(self.client, response)
+
+
+class HistoryPage(object):
+    def __init__(self, client, response):
+        self.client = client
+        self.response = response
+
+
+class LaunchPage(object):
+    def __init__(self, client, response):
+        self.client = client
+        self.response = response
+
+
+class RegisterPage(object):
+    def __init__(self):
+        self.url = '/cluster/register/'
--- cluster/tests.py
+++ cluster/tests.py
@@ -1,31 +1,33 @@
 """
-This is a placeholder.  Doctests should only be used as brief examples inline
-in the code.  Otherwise, most testing should be done as lettuce features, see
-the cluster/features directory.
+I would really like to be doing ATDD and working off of lettuce tests instead,
+but I don't feel that I have that figured out yet.  I am using this as a crutch
+at this point.
 """
 
 from django.test import TestCase
+from cluster.page_objects import LoginPage, RegisterPage, DashboardPage
 import re
 
 
 class SimpleTest(TestCase):
     def test_register_present(self):
-        response = self.client.get('/cluster/register/')
+        page = RegisterPage()
+        response = self.client.get(page.url)
         self.failUnlessEqual(response.status_code, 200)
 
     def test_login_present(self):
-        response = self.client.get('/accounts/login/')
-        self.failUnlessEqual(response.status_code, 200)
+        page = LoginPage(self.client)
+        page.visit()
+        self.failUnlessEqual(page.response.status_code, 200)
 
 
 class LoginTest(TestCase):
     fixtures = [ 'TestUser.json' ]
 
     def test_login_failure(self):
-        response = self.client.post('/accounts/login/', {
-                    'username': 'missinguser',
-                    'password': 'none',
-                })
+        page = LoginPage(self.client)
+        page.login('missinguser', 'badpass')
+        response = page.response
         self.assertEqual(response.status_code, 200)
         self.assertTrue(re.search(
                     r"(Username and password didn't match. Please try again.)",
@@ -34,10 +36,9 @@ class LoginTest(TestCase):
                     "A Failed login did not return the right message.")
 
     def test_login_success(self):
-        response = self.client.post('/accounts/login/?next=/', {
-                    'username': 'user001',
-                    'password': 'wombat',
-                })
+        page = LoginPage(self.client)
+        page.login('user001', 'wombat', '/')
+        response = page.response
         self.assertEqual(response.status_code,
                 302,
                 "User login probably failed."
@@ -53,28 +54,22 @@ class AccessTest(TestCase):
 
     # A user should be able to launch his own cluster
     def test_cluster_launch(self):
-        response = self.client.post('/accounts/login/?next=/', {
-                    'username': 'user001',
-                    'password': 'wombat',
-                })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['Location'], 'http://testserver/')
-        # FIXME: This should be a POST
-        response = self.client.get('/cluster/1/launch')
+        page = LoginPage(self.client)
+        page.login('user001', 'wombat')
+        dashboard_page = DashboardPage(self.client)
+        launch_page = dashboard_page.launch('1')
+        response = launch_page.response
         self.assertEqual(response.status_code, 200)
         match_string = "The following cluster is launching:"
         self.assertTrue(re.search(match_string, response.content))
  
     # A user should be able to access his own cluster history
     def test_cluster_history(self):
-        response = self.client.post('/accounts/login/?next=/', {
-                    'username': 'user001',
-                    'password': 'wombat',
-                })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['Location'], 'http://testserver/')
-        # FIXME: This should be a POST
-        response = self.client.get('/cluster/1/history')
+        page = LoginPage(self.client)
+        page.login('user001', 'wombat')
+        dashboard_page = DashboardPage(self.client)
+        history_page = dashboard_page.get_history('1')
+        response = history_page.response
         self.assertEqual(response.status_code, 200)
         match_string = "Cluster History for"
         self.assertTrue(re.search(match_string, response.content))
@@ -84,21 +79,27 @@ class AccessTest(TestCase):
     # A user should be able to edit his own Profile
 
     # A user should not be able to launch other clusters
-    def test_denied_cluster_launch(self):
-        response = self.client.post('/accounts/login/?next=/', {
-                    'username': 'user001',
-                    'password': 'wombat',
-                })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['Location'], 'http://testserver/')
-        # FIXME: This should be a POST
-        response = self.client.get('/cluster/2/launch')
-        #print response.content
+    def test_cluster_launch_denied(self):
+        page = LoginPage(self.client)
+        page.login('user001', 'wombat')
+        dashboard_page = DashboardPage(self.client)
+        launch_page = dashboard_page.launch('2')
+        response = launch_page.response
         self.assertEqual(response.status_code, 200)
         match_string = "Cluster Access Denied"
         self.assertTrue(re.search(match_string, response.content))
 
     # A user should not be able to access other cluster histories
+    def test_cluster_history_denied(self):
+        page = LoginPage(self.client)
+        page.login('user001', 'wombat')
+        dashboard_page = DashboardPage(self.client)
+        history_page = dashboard_page.get_history('2')
+        response = history_page.response
+        self.assertEqual(response.status_code, 200)
+        match_string = "Cluster History Access Denied"
+        self.assertTrue(re.search(match_string, response.content))
+
     # A user should not be able to access other SSH Keys
     # A user should not be able to access other Profiles
     # A user should not be able to edit other Profiles
